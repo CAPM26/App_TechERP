b8d08783c70b14b9b10dac2899023392fc480efe
Josue <jjimenezs8miumg.edu.gt>
2025-05-05 16:08:49 -0600

    cambio

--- firebase.js
+++ firebase.js
@@ -4,10 +4,21 @@ import {
   getFirestore,
   collection,
   addDoc,
-  getDocs
+  getDocs,
+  getDoc,
+  doc,
+  setDoc,
+  updateDoc,
+  deleteDoc
 } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
 
-// Configuración de Firebase
+// Configuración
 const firebaseConfig = {
   apiKey: "AIzaSyD1vKs3CSDUbvU3XaFHzX_hmdq05ga2TUE",
   authDomain: "erpj-3f6b7.firebaseapp.com",
@@ -17,8 +28,22 @@ const firebaseConfig = {
   appId: "1:277321931724:web:52b2aa01f80ac816109b69"
 };
 
-
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+export {
+  db,
+  auth,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  doc,
+  setDoc,
+  updateDoc,
+  deleteDoc,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+};
